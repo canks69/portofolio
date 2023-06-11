328fe8248d724831d05c4c17aefe71d3cdd51f72
San <ikhsanardiansyah659@gmail.com>
2023-06-12 02:18:33 +0700

    Push All

---
+++ app/layout.tsx
@@ -0,0 +1,77 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+	title: {
+		default: "Muh Ihsan Nur",
+		template: "%s | canks.dev",
+	},
+	description: "seorang Web Developer A2Z Solusindo Teknologi",
+	openGraph: {
+		title: "Muh Ihsan Nur",
+		description:
+			"seorang Web Developer A2Z Solusindo Teknologi",
+		url: "https://azsolusindo.info",
+		siteName: "Azsolusindo",
+		images: [
+			{
+				url: "https://azsolusindo.info/og.png",
+				width: 1920,
+				height: 1080,
+			},
+		],
+		locale: "en-US",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	twitter: {
+		title: "Muh Ihsan Nur",
+		card: "summary_large_image",
+	},
+	icons: {
+		shortcut: "/favicon.png",
+	},
+};
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+	src: "../public/fonts/CalSans-SemiBold.ttf",
+	variable: "--font-calsans",
+});
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+			<head>
+				<Analytics />
+			</head>
+			<body
+				className={`bg-black ${
+					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+				}`}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
